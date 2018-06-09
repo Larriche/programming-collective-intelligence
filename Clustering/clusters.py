74e62c16b5cc76856a0bdd95b0cfe6c6e302b719
Richman Larry Clifford <larriche99@gmail.com>
2018-06-09 15:37:30 +0000

    Started adding function for k-means clustering

--- Clustering/clusters.py
+++ Clustering/clusters.py
@@ -1,3 +1,4 @@
+import random
 from math import sqrt
 from PIL import Image, ImageDraw
 
@@ -95,6 +96,59 @@ def hcluster(rows, distance = pearson):
 
     return clusters[0]
 
+def kcluster(rows, distance=pearson, k=4):
+    # Determine the minimum and maximum values for each point
+    # This is the min and max row value for each column
+    ranges = [(min([row[i] for row in rows]), max([row[i] for row in rows])) for i in range(len(rows[0]))]
+
+    # Create k randomly placed centroids that are within range of each row
+    clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0] for i in range(len(rows[0]))] for j in range(k)]
+
+    lastmatches = None
+
+    for t in range(100):
+        # 100 is an arbitrarily chosen number of iterations we want to do
+        # if we are not able to attain the desired number of clusters soon
+        # enough
+        print 'Iteration %d' % t
+        bestmatches = [[] for i in range(k)]
+
+        # Find which centroid is the closest for each row
+        for j in range(len(rows)):
+            row = rows[j]
+            bestmatch = 0
+
+            for i in range(k):
+                d = distance(clusters[i], row)
+
+                if d < distance(clusters[bestmatch], row):
+                    bestmatch = i
+
+            bestmatches[bestmatch].append(j)
+
+        # If the results are the same as the last time, this is complete
+        if bestmatches == lastmatches:
+            break
+
+        lastmatches = bestmatches
+
+        # Move the centroids to the average of their members
+        for i in range(k):
+            avgs = [0.0] * len(rows[0])
+
+            if len(bestmatches[i]) > 0:
+                for rowid in bestmatches[i]:
+                    for m in range(len(rows[rowid])):
+                        avgs[m] += rows[rowid][m]
+
+                    for j in range (len(avgs)):
+                        avgs[j] /= len(bestmatches[i])
+
+                clusters[i] = avgs
+
+    return bestmatches
+
+
 def print_cluster(cluster, labels=None, n=0):
     # indent to make hierarchy layout
     for i in range(n):
@@ -178,4 +232,6 @@ def draw_node(draw, cluster, x, y, scaling, labels):
 #print read_file('blogdata.txt')
 blognames, words, data = read_file('blogdata.txt')
 cluster = hcluster(data)
-draw_dendrogram(cluster, blognames, jpeg='blogdata.jpg')
+kcluster = kcluster(data,k=10)
+print kcluster
+#draw_dendrogram(cluster, blognames, jpeg='blogdata.jpg')
