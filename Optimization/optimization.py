8ed22adfc5b2d8c22bef60bd6a6a3ee6a2824cac
Richman Larry Clifford <larriche99@gmail.com>
2018-06-15 21:21:00 +0000

    Added function for simulated annealing

--- Optimization/optimization.py
+++ Optimization/optimization.py
@@ -78,7 +78,7 @@ def randomoptimize(domain, costf):
 
 def hillclimb(domain, costf):
     """
-    Uses a hillclimbing approach to looking for a solution
+    Uses a hillclimbing approach to look for a solution
     """
     # Create a random solution
     sol = [random.randint(domain[i][0], domain[i][1]) for i in range(len(domain))]
@@ -111,6 +111,38 @@ def hillclimb(domain, costf):
 
         return sol
 
+def annealingoptimize(domain, costf, T=10000.0, cool=0, step=1):
+    # Initialize the values randomly
+    vec = [float(random.randint(domain[i][0], domain[i][1])) for i in range(len(domain))]
+
+    while T > 0.1:
+        # Choose one of the indices
+        i = random.randint(0, len(domain)-1)
+
+        # Choose a direction to change it
+        dir = random.randint(-step, step)
+
+        # Create a new list with one of the values changed
+        vecb = vec[:]
+        vecb[i] += dir
+
+        if vecb[i] < domain[i][0]: vecb[i] = domain[i][0]
+        elif vecb[i] > domain[i][1]: vecb[i] = domain[i][1]
+
+        # Calculate the current cost and the new cost
+        ea = costf(vec)
+        eb = costf(vecb)
+        p = pow(math.e, (-eb-ea)/T)
+
+        # Is it better, or does it make the probability cutoff?
+        if (eb<ea or random.random()<p):
+            vec = vecb
+
+        # Decrease the temperature
+        T=T*cool
+
+    return vec
+
 people = [('Seymour', 'BOS'),
     ('Franny', 'DAL'),
     ('Zooey', 'CAK'),
@@ -134,4 +166,7 @@ s = randomoptimize(domain, schedulecost)
 print schedulecost(s)
 
 s = hillclimb(domain, schedulecost)
+print schedulecost(s)
+
+s = annealingoptimize(domain, schedulecost)
 print schedulecost(s)
